df3764a9f0730f443ae0fdc4d47814048450076d
ancavar <danaj2004@yandex.ru>
2024-01-12 10:32:48 +0300

    feat: add custom `pp_type` for typedtree

--- Haskell/lib/typedtree.ml
+++ Haskell/lib/typedtree.ml
@@ -1,12 +1,16 @@
+(** Copyright 2023-2024, Danil P *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
 type binder = int [@@deriving show { with_path = false }]
 
 module VarSet = struct
   include Stdlib.Set.Make (Int)
 
-  let pp ppf s =
-    Format.fprintf ppf "[ ";
-    iter (Format.fprintf ppf "%d; ") s;
-    Format.fprintf ppf "]"
+  let pp fmt s =
+    Format.fprintf fmt "[ ";
+    iter (Format.fprintf fmt "%d; ") s;
+    Format.fprintf fmt "]"
   ;;
 end
 
@@ -20,14 +24,28 @@ type ty =
   | TyTuple of ty list
 [@@deriving show { with_path = false }]
 
-
 type scheme = S of binder_set * ty [@@deriving show { with_path = false }]
 
-let arrow l r = TyArrow (l, r)
-let int_typ = TyLit "int"
-let bool_typ = TyLit "bool"
-let v x = TyVar x
-let list_typ t = TyList t
-let tuple_typ ts = TyTuple ts
-
-let ( @-> ) = arrow
+let pp_type fmt ty =
+  let open Format in
+  let rec helper fmt = function
+    | TyVar n -> fprintf fmt "p%d" n
+    | TyLit s -> pp_print_string fmt s
+    | TyArrow (l, r) ->
+      (match l with
+       | TyArrow (_, _) -> fprintf fmt "(%a) -> %a" helper l helper r
+       | _ -> fprintf fmt "%a -> %a" helper l helper r)
+    | TyList t -> fprintf fmt "[%a]" helper t
+    | TyTuple ts ->
+      fprintf fmt "(";
+      fprintf
+        fmt
+        "%a"
+        (pp_print_list
+           ~pp_sep:(fun fmt () -> fprintf fmt ", ")
+           (fun fmt t -> fprintf fmt "%a" helper t))
+        ts;
+      fprintf fmt ")"
+  in
+  helper fmt ty
+;;
