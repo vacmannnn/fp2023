5a501b677e6b27a0cf2f8e9a05593dc735c63fe8
Alexandr <opera9876@rambler.ru>
2024-01-13 05:01:21 +0300

    interpret while, return, operations

--- Lua/lib/interpreter/interpreter.ml
+++ Lua/lib/interpreter/interpreter.ml
@@ -133,48 +133,36 @@ module Eval (M : MONADERROR) = struct
     | Exp_nil ->
         return Exp_nil
     | Exp_string s ->
-        return @@ Exp_string s
+        return (Exp_string s)
     | Exp_op (op, lhs, rhs) -> (
-      match op with
-      | Op_add | Op_sub | Op_mul | Op_div | Op_mod ->
-          let _ = function
-            | Op_add ->
-                (( +. ), "Error: Unsupported operands type for (+)")
-            | Op_sub ->
-                (( -. ), "Error: Unsupported operands type for (-)")
-            | Op_mul ->
-                (( *. ), "Error: Unsupported operands type for (*)")
-            | Op_div ->
-                (( /// ), "Error: Unsupported operands type for (//)")
-            | _ ->
-                (( %% ), "Error: Unsupported operands type for (%)")
-          in
-          eval_expr env_lst lhs
-          (* >>= fun e_lhs ->
-             eval_expr env_lst rhs
-             >>= fun e_rhs ->
-             match get_op op with
-             | op, err_msg ->
-                 transform_to_number e_lhs e_rhs err_msg
-                 >>= fun (x, y) -> return @@ VNumber (op x y) ) ) *)
-      | Op_eq | Op_lt | Op_le ->
-          eval_expr env_lst lhs
-          >>= fun e_lhs ->
-          eval_expr env_lst rhs >>= fun e_rhs -> compare e_lhs e_rhs op
-      | Op_and | Op_or ->
-          let get_op = function Op_and -> ( && ) | _ -> ( || ) in
-          eval_expr env_lst lhs
-          >>= fun e_lhs ->
-          eval_expr env_lst rhs
-          >>= fun e_rhs ->
-          if (get_op op) (is_true e_lhs) (is_true e_rhs) then return Exp_true
-          else return Exp_false
-      | Op_not ->
-          return Exp_nil
-      | Op_concat ->
-          return Exp_nil
-      | Op_len ->
-          return Exp_nil )
+        eval_expr env_lst lhs
+        >>= fun l ->
+        eval_expr env_lst rhs
+        >>= fun r ->
+        match op with
+        | Op_le | Op_lt | Op_eq ->
+            compare l r op
+        | Op_add | Op_mul | Op_sub | Op_div -> (
+            let get_op = function
+              | Op_add ->
+                  (( +. ), "Error: Unsupported operands type for (+)")
+              | Op_sub ->
+                  (( -. ), "Error: Unsupported operands type for (-)")
+              | Op_mul ->
+                  (( *. ), "Error: Unsupported operands type for (*)")
+              | Op_div ->
+                  (( /. ), "Error: Unsupported operands type for (/)")
+              | Op_mod ->
+                  (( %% ), "Error: Unsupported operands type for (%)")
+              | _ ->
+                  (( +. ), "Бля сюда не попасть")
+            in
+            match get_op op with
+            | op, err_msg ->
+                arg_to_num l r err_msg
+                >>= fun (x, y) -> return (Exp_number (op x y)) )
+        | _ ->
+            return Exp_nil )
     | Exp_lhs l ->
         find_var l env_lst
     | _ ->
@@ -214,11 +202,61 @@ module Eval (M : MONADERROR) = struct
         error "Error: Unexpected return statement"
     | Stat_break ->
         error "Error: Unexpected break statement"
+    | Stat_while (e, bal) ->
+        set_hd_is_loop env_lst >>= fun env_lst -> eval_while e bal env_lst
     | _ ->
         error "sad"
 
+  and eval_while e bal env_lst =
+    eval_expr env_lst e
+    >>= fun predicate ->
+    if is_true predicate then
+      eval_block env_lst bal
+      >>= fun env_lst ->
+      get_cur_env env_lst
+      >>= fun cur_env ->
+      match cur_env.jump_stmt with
+      | Break ->
+          set_hd_jump_stmt Default env_lst
+      | Return ->
+          set_hd_jump_stmt Return env_lst
+      | _ ->
+          eval_while e bal env_lst
+    else set_parents_is_loop env_lst
+
+  and set_parents_is_loop env_lst =
+    let prev_env = get_prev_env env_lst in
+    let prev_is_loop =
+      match prev_env with [] -> false | _ -> (List.hd prev_env).is_loop
+    in
+    set_hd_is_loop ~is_loop:prev_is_loop env_lst
+
+  and set_hd_is_loop ?(is_loop = true) = function
+    | [] ->
+        error
+          "Error: Can't modify environment head with <is_loop>. Head is absent!"
+    | hd :: tl ->
+        return @@ ({hd with is_loop} :: tl)
+
   and eval_vardec local_flag env_lst (lhs, exp) =
     eval_expr env_lst exp >>= fun v -> assign lhs v local_flag env_lst
+
+  and eval_return env_lst e =
+    eval_expr env_lst e
+    >>= fun v ->
+    let prev_env = get_prev_env env_lst in
+    set_hd_last_value v prev_env
+    >>= fun prev_env -> set_hd_jump_stmt Return prev_env
+
+  and set_hd_jump_stmt jump_stmt = function
+    | [] ->
+        error
+          "Error: Can't modify environment head with <jump_stmt>. Head is \
+           absent!"
+    | hd :: tl ->
+        return @@ ({hd with jump_stmt} :: tl)
+
+  and get_prev_env = function [] -> [] | _ :: tl -> tl
 
   and create_next_env = function
     | [] ->
@@ -245,61 +283,65 @@ module Eval (M : MONADERROR) = struct
     in
     if is_global loc then (set_global n v env_lst ; return env_lst)
     else modify_hd_vars (n, v) env_lst >>= fun _ -> return env_lst
-  (*
-     and eval_block env_lst = function
-       | [] ->
-           return @@ get_prev_env env_lst
-       | [tl] -> (
-           get_cur_env env_lst
-           >>= fun cur_env ->
-           match tl with
-           | Stat_return v when cur_env.is_func ->
-               eval_return env_lst v
-           | Stat_break when cur_env.is_loop ->
-               eval_break env_lst
-           | Stat_break ->
-               error "Error: Unexpected break statement"
-           | Stat_return _ ->
-               error "Error: Unexpected return statement"
-           | _ -> (
-               eval_stmt env_lst tl
-               >>= fun env_lst ->
-               get_cur_env env_lst
-               >>= fun cur_env ->
-               match cur_env.jump_stmt with
-               | Return ->
-                   let prev_env = get_prev_env env_lst in
-                   set_hd_last_value cur_env.last_value prev_env
-                   >>= fun prev_env -> set_hd_jump_stmt Return prev_env
-               | Break ->
-                   eval_break env_lst
-               | _ -> (
-                   let prev_env = get_prev_env env_lst in
-                   match prev_env with
-                   | [] ->
-                       return env_lst
-                   | _ ->
-                       return prev_env ) ) )
-       | hd :: tl -> (
-           eval_stmt env_lst hd
-           >>= fun env_lst ->
-           get_cur_env env_lst
-           >>= fun cur_env ->
-           match cur_env.jump_stmt with
-           | Return ->
-               let prev_env = get_prev_env env_lst in
-               set_hd_last_value cur_env.last_value prev_env
-               >>= fun prev_env -> set_hd_jump_stmt Return prev_env
-           | Break ->
-               eval_break env_lst
-           | _ ->
-               eval_block env_lst tl ) *)
+
+  and eval_block env_lst = function
+    | [] ->
+        return @@ get_prev_env env_lst
+    | [tl] -> (
+        get_cur_env env_lst
+        >>= fun cur_env ->
+        match tl with
+        (* | Stat_return v when cur_env.is_func ->
+            eval_return env_lst v *)
+        | Stat_break when cur_env.is_loop ->
+            eval_break env_lst
+        | Stat_break ->
+            error "Error: Unexpected break statement"
+        | Stat_return _ ->
+            error "Error: Unexpected return statement"
+        | _ -> (
+            eval_stmt env_lst tl
+            >>= fun env_lst ->
+            get_cur_env env_lst
+            >>= fun cur_env ->
+            match cur_env.jump_stmt with
+            | Return ->
+                let prev_env = get_prev_env env_lst in
+                set_hd_last_value cur_env.last_value prev_env
+                >>= fun prev_env -> set_hd_jump_stmt Return prev_env
+            | Break ->
+                eval_break env_lst
+            | _ -> (
+                let prev_env = get_prev_env env_lst in
+                match prev_env with
+                | [] ->
+                    return env_lst
+                | _ ->
+                    return prev_env ) ) )
+    | hd :: tl -> (
+        eval_stmt env_lst hd
+        >>= fun env_lst ->
+        get_cur_env env_lst
+        >>= fun cur_env ->
+        match cur_env.jump_stmt with
+        | Return ->
+            let prev_env = get_prev_env env_lst in
+            set_hd_last_value cur_env.last_value prev_env
+            >>= fun prev_env -> set_hd_jump_stmt Return prev_env
+        | Break ->
+            eval_break env_lst
+        | _ ->
+            eval_block env_lst tl )
+
+  and eval_break env_lst =
+    let prev_env = get_prev_env env_lst in
+    set_hd_jump_stmt Break prev_env >>= fun env -> set_parents_is_loop env
 
   and eval_prog = function
     | None ->
         error "Syntax error occured"
     | Some p ->
-        eval_stmt (create_next_env []) p
+        eval_block (create_next_env []) p
 end
 
 open Eval (Result)
@@ -315,8 +357,25 @@ let eval parsed_prog =
   | Error msg ->
       print_endline msg
 
+let%expect_test "help" =
+  eval
+    (Some
+       [ Stat_assign (Nonlocal, "x", Exp_number 15.)
+       ; Stat_while
+           ( Exp_op (Op_lt, Exp_number 10., Exp_lhs "x")
+           , [ Stat_assign
+                 (Nonlocal, "x", Exp_op (Op_sub, Exp_lhs "x", Exp_number 4.)) ]
+           ) ] ) ;
+  [%expect
+    {|
+      { vars = [["x" -> (Exp_number 7.)
+
+      ]]
+      ; last_value = Exp_nil; is_func = false; is_loop = false; jump_stmt = Default
+      } |}]
+
 let%expect_test "abc" =
-  eval (Some (Stat_assign (Nonlocal, "x", Exp_number 42.))) ;
+  eval (Some [Stat_assign (Nonlocal, "x", Exp_number 42.)]) ;
   [%expect
     {|
     { vars = [["x" -> (Exp_number 42.)
@@ -326,7 +385,7 @@ let%expect_test "abc" =
     } |}]
 
 let%expect_test "abc" =
-  eval (Some (Stat_assign (Nonlocal, "x", Exp_string "whennnnnn"))) ;
+  eval (Some [Stat_assign (Nonlocal, "x", Exp_string "whennnnnn")]) ;
   [%expect
     {|
     { vars = [["x" -> (Exp_string "whennnnnn")
@@ -337,7 +396,11 @@ let%expect_test "abc" =
 
 let%expect_test "abc" =
   eval
-    (Some (Stat_while (Exp_false, [Stat_assign (Nonlocal, "x", Exp_number 1.)]))) ;
-  [%expect {| sad |}]
+    (Some [Stat_while (Exp_false, [Stat_assign (Nonlocal, "x", Exp_number 1.)])]) ;
+  [%expect
+    {|
+    { vars = [[]]
+      ; last_value = Exp_nil; is_func = false; is_loop = false;
+      jump_stmt = Default } |}]
 
 let%expect_test "123" = eval None ; [%expect {| Syntax error occured |}]
